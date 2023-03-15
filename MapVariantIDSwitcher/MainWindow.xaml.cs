9f57d85a2ce55e1c63f4d77ef7761cc56db3a349
Krevil <krevil.3000@gmail.com>
2023-03-15 22:57:20 +0000

    Add project files.

---
+++ MapVariantIDSwitcher/MainWindow.xaml.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Ookii.Dialogs.Wpf;
+using System.IO;
+using System.Text.Json;
+using System.ComponentModel;
+using System.Windows.Controls;
+using MapVariantIDSwitcher.Windows;
+
+namespace MapVariantIDSwitcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public HaloEngineManager haloEngineManager = new HaloEngineManager();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = haloEngineManager;
+        }
+
+        private void InputModButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Map JSON (.json)|*.json"
+            };
+            if (Properties.Settings.Default.DefaultModMapPath.Length > 0)
+            {
+                openFileDialog.InitialDirectory = Properties.Settings.Default.DefaultModMapPath;
+            }
+            if (openFileDialog.ShowDialog() == true) 
+            {
+                InputModMap.Text = openFileDialog.FileName;
+                OutputTextBlock.Text += MapVariantManager.LoadModMap(openFileDialog.FileName);
+            }
+        }
+
+        private void InputVariantButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Map Variant (.mvar)|*.mvar"
+            };
+            if (Properties.Settings.Default.DefaultMapVariantPath.Length > 0)
+            {
+                openFileDialog.InitialDirectory = Properties.Settings.Default.DefaultMapVariantPath;
+            }
+            if (openFileDialog.ShowDialog() == true)
+            {
+                InputMapVariant.Text = openFileDialog.FileName;
+                OutputTextBlock.Text += MapVariantManager.LoadMapVariant(openFileDialog.FileName);
+            }
+        }
+
+        private void OutputFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog vistaFolderBrowserDialog = new VistaFolderBrowserDialog();
+            if (Properties.Settings.Default.DefaultOutputPath.Length > 0)
+            {
+                vistaFolderBrowserDialog.SelectedPath = Properties.Settings.Default.DefaultOutputPath;
+            }
+            if (vistaFolderBrowserDialog.ShowDialog() == true) 
+            {
+                OutputFolder.Text = vistaFolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void ProcessVariantButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (HaloEngineManager.SelectedEngine == null)
+            {
+                MessageBox.Show("You must select a game engine");
+                return;
+            }
+            if (InputModMap.Text.Length <= 0)
+            {
+                MessageBox.Show("You must select an input mod json file");
+                return;
+            }
+            if (InputMapVariant.Text.Length <= 0)
+            {
+                MessageBox.Show("You must select an input map variant file");
+                return;
+            }
+            if (OutputFolder.Text.Length <= 0)
+            {
+                MessageBox.Show("You must select an output folder");
+                return;
+            }
+            if (MapVariantManager.SwitchMapIds(InputMapVariant.Text, OutputFolder.Text) == true)
+            {
+                OutputTextBlock.Text += "Successfully switched variant " + MapVariantManager.ManagedMapVariant.Name + " to use " + MapVariantManager.ModMap.Title.GetValueOrDefault("Neutral") + "\n";
+            }
+        }
+
+        private void OutputTextBlock_Initialized(object sender, EventArgs e)
+        {
+            OutputTextBlock.Text += "Application loaded\n";
+        }
+
+        private void EngineComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            HaloEngineManager.SelectedEngine = (HaloEngine)((ComboBox)sender).SelectedItem;
+            Properties.Settings.Default.DefaultEngine = EngineComboBox.SelectedIndex;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            if (!File.Exists("Excession.dll") || !File.Exists("ManagedUFL.dll") || !File.Exists("Ijwhost.dll"))
+            {
+                MessageBoxResult mbResult = MessageBox.Show("This application must be placed in the same folder as the Halo MCC Mod Uploader (ExcessionApp) application and Dll files in order to function correctly.", "Error", MessageBoxButton.OKCancel);
+                if (mbResult != MessageBoxResult.OK) 
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+            else
+            {
+                OutputTextBlock.Text += "Loaded Excession Dlls\n";
+            }
+        }
+
+        private void Preferences_Click(object sender, RoutedEventArgs e)
+        {
+            Preferences preferences = new Preferences();
+            preferences.Show();
+        }
+
+        private void EngineComboBox_Initialized(object sender, EventArgs e)
+        {
+            EngineComboBox.SelectedIndex = Properties.Settings.Default.DefaultEngine;
+        }
+    }
+
+    public class HaloEngine
+    {
+        private Excession.HaloShellInterface.ManagedGameEngineDll _engine;
+        public Excession.HaloShellInterface.ManagedGameEngineDll Engine
+        {
+            get { return _engine; }
+            set { _engine = value; }
+        }
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string DllName;
+
+        public HaloEngine(Excession.HaloShellInterface.ManagedGameEngineDll engine, string name, string dllName)
+        {
+            _engine = engine;
+            _name = name;
+            DllName = dllName;
+            engine.LoadDll("bin\\" + dllName + ".dll");
+        }
+    }
+
+
+    public class HaloEngineManager
+    {
+        private static Excession.HaloShellInterface.ManagedGameEngineDll HaloReachDll = new Excession.HaloShellInterface.ManagedGameEngineDll(ManagedUFL.EGameEngine.HaloReach);
+        private static Excession.HaloShellInterface.ManagedGameEngineDll Halo4Dll = new Excession.HaloShellInterface.ManagedGameEngineDll(ManagedUFL.EGameEngine.Halo4);
+        private static Excession.HaloShellInterface.ManagedGameEngineDll Halo2ADll = new Excession.HaloShellInterface.ManagedGameEngineDll(ManagedUFL.EGameEngine.Halo2A);
+        private static Excession.HaloShellInterface.ManagedGameEngineDll Halo3Dll = new Excession.HaloShellInterface.ManagedGameEngineDll(ManagedUFL.EGameEngine.Halo3);
+
+        public List<HaloEngine> HaloEngines { get; set; } = new List<HaloEngine>();
+        public static HaloEngine? SelectedEngine;
+        
+        public HaloEngineManager()
+        {
+            HaloEngines.Add(new HaloEngine(HaloReachDll, "Halo Reach", "haloreach"));
+            HaloEngines.Add(new HaloEngine(Halo4Dll, "Halo 4", "halo4"));
+            HaloEngines.Add(new HaloEngine(Halo2ADll, "Halo 2 Anniversary", "groundhog"));
+            HaloEngines.Add(new HaloEngine(Halo3Dll, "Halo 3", "halo3"));
+        }
+    }
+
+    public class ModMapJson
+    {
+        public Guid MapGuid { get; set; }
+        public string? ScenarioFile { get; set; }
+        public Dictionary<string, string>? Title { get; set; }
+        //public Dictionary<string, string>? Description { get; set; }
+        /*
+        public Dictionary<string, string>? Images { get; set; }
+        public string[]? Flags { get; set; }
+        public string[]? InsertionPoints { get; set; }
+        public Dictionary<string, string>? MaximumTeamsByGameCategory { get; set; }
+        public int[]? ValidMultiplayerObjectTypes { get; set; }
+        public string? MapDefaultPrimaryWeapon { get; set; }
+        public string? MapDefaultPrimaryWeaponForge { get; set; }
+        */
+    }
+
+    public class MapVariantManager
+    {
+        public static ManagedUFL.ManagedMapVariant? ManagedMapVariant;
+        public static ModMapJson? ModMap;
+
+        public static string? LoadMapVariant(string mvarPath)
+        {
+            if (HaloEngineManager.SelectedEngine == null) 
+            {
+                MessageBox.Show("Could not select an engine to use");
+                return null;
+            }
+            ManagedMapVariant = HaloEngineManager.SelectedEngine.Engine.CreateMapVariantFromFile(mvarPath);
+
+            return "Loaded " + ManagedMapVariant.Name + "\n";
+        }
+
+        public static string? LoadModMap(string modMapPath)
+        {
+            string modMapJsonText = "";
+            using (FileStream fs = new FileStream(modMapPath, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    modMapJsonText = sr.ReadToEnd();
+                }
+            }
+            ModMap = JsonSerializer.Deserialize<ModMapJson>(modMapJsonText);
+            if (ModMap == null)
+            {
+                return null;
+            }
+            if (ModMap.Title == null)
+            { 
+                return null;
+            }
+            else
+            {
+                string? modMapTitle;
+                ModMap.Title.TryGetValue("Neutral", out modMapTitle);
+                return "Loaded " + modMapTitle + "\n";
+            }
+        }
+
+        public static bool SwitchMapIds(string mvarPath, string outputPath)
+        {
+            if (HaloEngineManager.SelectedEngine == null)
+            {
+                MessageBox.Show("Could not select an engine to use");
+                return false;
+            }
+
+            if (ManagedMapVariant == null) 
+            {
+                MessageBox.Show("You must load a map variant before you can switch map Ids");
+                return false;
+            }
+
+            if (ModMap == null)
+            {
+                MessageBox.Show("Could not parse mod JSON file");
+                return false;
+            }
+            
+            ManagedUFL.ManagedHaloMapId targetMapId = ManagedUFL.ManagedHaloMapId.FromGuid(ModMap.MapGuid);
+            ManagedMapVariant.ForceSetHaloMapId(targetMapId);
+            HaloEngineManager.SelectedEngine.Engine.SaveVariantToFile(ManagedMapVariant, Path.Combine(outputPath, Path.GetFileName(mvarPath)));
+            return true;
+        }
+    }
+}
---
+++ MapVariantIDSwitcher/Windows/Preferences.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Ookii.Dialogs.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MapVariantIDSwitcher.Windows
+{
+    /// <summary>
+    /// Interaction logic for Preferences.xaml
+    /// </summary>
+    public partial class Preferences : Window
+    {
+        public Preferences()
+        {
+            InitializeComponent();
+        }
+
+        private void SetMapVariantPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog folderBrowserDialog = new VistaFolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == true)
+            {
+                DefaultMapVariantPathTextBlock.Text = folderBrowserDialog.SelectedPath;
+                Properties.Settings.Default.DefaultMapVariantPath = folderBrowserDialog.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void SetModMapPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog folderBrowserDialog = new VistaFolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == true)
+            {
+                DefaultModMapPathTextBlock.Text = folderBrowserDialog.SelectedPath;
+                Properties.Settings.Default.DefaultModMapPath = folderBrowserDialog.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void SetOutputFolderPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog folderBrowserDialog = new VistaFolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == true)
+            {
+                DefaultOutputFolderPathTextBlock.Text = folderBrowserDialog.SelectedPath;
+                Properties.Settings.Default.DefaultOutputPath = folderBrowserDialog.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+    }
+}
